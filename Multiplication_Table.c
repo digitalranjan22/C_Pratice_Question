a19cfffd3b256ebd13f072d4b93130664a8e339d
Digital Ranjan <ranjanrajdeo10@gmail.com>
2022-06-19 08:31:31 +0530

    c Pratice

---
+++ Multiplication_Table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    // Take input
+    int num;
+    printf("Enter No\n");
+    scanf("%d",&num);
+    printf("The Table of %d is \n",num);
+    for (int i= 1; i <10 ; i++)
+    {
+        printf("%d X %d =%d\n",num,i,i*num);
+    }
+    return 0;
+}
