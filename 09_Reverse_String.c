43ccbaea458e557620b4ea6ad1f5d9623734cba3
Digital Ranjan <ranjanrajdeo10@gmail.com>
2022-06-19 21:36:31 +0530

    rev_str

---
+++ 09_Reverse_String.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    char s[] ="Digital";
+    char temp;
+    int len = 0;
+    while(s[len]!='\0'){
+        len ++;
+    }
+    printf("the length is %d \n",len);
+    for(int i = 0; i<(len-1)/2;i++)
+    {
+        temp = s[i];
+        s[i]= s[len-1-i];
+        s[len-1-i] = temp ;
+    }
+    printf("String now is %s",s);
+    return 0;
+}
