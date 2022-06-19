2f707c50782618337ed7da1a2e1beeb87f7fc8ff
Digital Ranjan <ranjanrajdeo10@gmail.com>
2022-06-19 21:01:04 +0530

    q fact

--- 07_Factorial_Number.c
+++ 07_Factorial_Number.c
@@ -4,12 +4,12 @@ int main(){
     int num ,fact = 1;
     printf("Enter no: ");// Take input
     scanf("%d",&num);// store input
-    printf("The fact of  %d",num);
+    // printf("The fact of  %d",num);
     // 5! = 5*4*3*2*1
     for(int i=num ;i >1 ; i--)
     {
         fact = fact * i;
     }
-    printf("The fact of %d",fact);
+    printf("The fact of %d is %d ",num,fact);
     return 0;
 }
